c47c0611bd2fa51373583dbaa442a9ab88706cf7
kgrzegor <konradgrzegorczyk97@gmail.com>
2022-10-12 20:58:37 +0200

    Init

---
+++ backend/src/main/kotlin/us/feed/Feed/Us/FeedUsApplication.kt
@@ -0,0 +1,11 @@
+package us.feed.Feed.Us
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FeedUsApplication
+
+fun main(args: Array<String>) {
+	runApplication<FeedUsApplication>(*args)
+}
---
+++ backend/src/test/kotlin/us/feed/Feed/Us/FeedUsApplicationTests.kt
@@ -0,0 +1,13 @@
+package us.feed.Feed.Us
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FeedUsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
